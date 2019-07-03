a06be2a4285ffaeacb90536a557b7188b20dd2f2
Alan Anderson <alan.anderson@udacity.com>
2019-07-02 20:53:57 -0700

    Add PostController WIP

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+//        if ($request->user()->canPost || true){
+            return view('posts.create');
+//        } else {
+//            return redirect('/')->withErrors('You do not have sufficient permissions to writing posts');
+//        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->get('title');
+        $post->body = $request->get('body');
+        $post->slug = str_slug($post->title);
+        $post->author_id = $request->user()->id;
+        if ($request->has('save')){
+            $message = 'Post saved successfully';
+        } else {
+            $post->published_at = date();
+            $message = 'Post published successfully';
+        }
+        dd($post);
+        $post->save();
+        //return redirect('edit/'.$post->slug)->withMessage($message);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        dd(Post::all());
+        $post = Post::whereSlug($slug)->first();
+        if (!$post){
+            return redirect('/')->withErrors('requested page not found');
+        }
+        return view('posts.show')->withPost($post)->withComments($post->comments);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
