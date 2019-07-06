323be161434cf7634c3948b544208b4cfc90cc65
Alan Anderson <alan.anderson@udacity.com>
2019-07-05 23:05:28 -0700

    Add framework for showing a post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function author() {
+        return $this->belongsTo(User::class);
+    }
 }
